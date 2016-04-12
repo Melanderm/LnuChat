34e05390462c63d21feed44c1d7604d75e7307d5
Mikael Melander <mm222ev@student.lnu.se>
2016-04-12 15:37:03 +0200

    Adding CloudCode automatic color selection

--- Project/Cloudcode/cloud/main.js
+++ Project/Cloudcode/cloud/main.js
@@ -3,14 +3,53 @@ Created by Mikael Melander 2016-03-24
 
 CloudCode functions
  */
+ /*
+Before anything is saved in ChatRooms this script is executed.
 
+Set of basic colors a room can be set to.
+
+Querys the latest room and fetches its color.
+
+Compares it to the random color the room will be assigned.
+If they are the same, gets a new random color until they are diffrent.
+So rooms that are next to each other cant be the same color. I mean how ugly? :P
+ */
+Parse.Cloud.beforeSave("ChatRooms", function(request, response) {
+
+    Parse.Cloud.useMasterKey();
+    var colors = ["#2ecc71", "#3498db", "#9b59b6", "#34495e","#1abc9c", "#e67e22","#ea6153", "#7f8c8d"];
+    var numb = Math.floor((Math.random() * colors.length));
+    var color = colors[numb];
+
+    var Ca = Parse.Object.extend("ChatRooms");
+    var ca = new Ca();
+    var query = new Parse.Query(ca);
+    query.descending("createdAt");
+    query.limit(1);
+    query.find({
+        success:function(object){
+            var prev = object[0].get("color");
+            while (prev == color) {
+                numb = Math.floor((Math.random() * colors.length));
+                color = colors[numb];
+            }
+                request.object.set("color", color);
+                response.success();
+
+        },error:function(){
+            response.error(error);
+        }
+
+    });
+});
 
 Parse.Cloud.define("licensText", function(request, response) {
 
-    var textLicens = "When using this application you are not allowed to use foul language, you need to follow the law, not post copyrighted material.\nShow each other respect.  \nYou also agree that we store your name in a remote database.\n\nCreated by Mikael Melander for Linnaeus university Course 1DV430";
+   // var textLicens = "When using this application you are not allowed to use foul language, you need to follow the law, not post copyrighted material.\nShow each other respect.  \nYou also agree that we store your name in a remote database.\n\nCreated by Mikael Melander for Linnaeus university Course 1DV430";
 
+    var textLicens = "More text to come...\nYou agree that we store your name in a remote database. According to PUL \n\nCreated by Mikael Melander for Linnaeus university Course 1DV430";
     response.success(textLicens);
-});
+});;
 
 /*
  Creates user object
