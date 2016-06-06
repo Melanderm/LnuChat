9f38398d4119755ad7d3539930f4221427b9707a
Mikael <mm222ev@student.lnu.se>
2016-06-06 17:10:32 +0200

    Adding presentation video

--- Project/App/LnuChat/ViewController.m
+++ Project/App/LnuChat/ViewController.m
@@ -383,7 +383,7 @@
                     
                              UIAlertController * alert2=   [UIAlertController
                                                            alertControllerWithTitle:NSLocalizedString(@"ROOMTYPE", @"")
-                                                           message:NSLocalizedString(@"ROOMTYPEDESCRIPTEION", @"")
+                                                           message:NSLocalizedString(@"ROOMTYPEDESCRIPTION", @"")
                                                            preferredStyle:UIAlertControllerStyleAlert];
                              
                              UIAlertAction* ok2 = [UIAlertAction
